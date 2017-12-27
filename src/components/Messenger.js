f804542bf69824b4a765d8ab98dda1eb79a6ebdb
ryan-qiyu-jiang <ryan.qiyu.jiang@gmail.com>
2017-12-27 02:10:43 -0500

    more natural language proccess, context, scroll bar, new atomicy message state update

--- src/components/Messenger.js
+++ src/components/Messenger.js
@@ -4,12 +4,28 @@ import keyIndex from 'react-key-index';
 import Message from "../components/Message";
 
 export default class Messenger extends React.Component{
+scrollToBottom() {
+  this.messagesEnd.scrollIntoView({ behavior: "smooth" });
+}
+
+componentDidMount() {
+  this.scrollToBottom();
+}
+
+componentDidUpdate() {
+  this.scrollToBottom();
+  console.log("*************messenger updated");
+}
+
 render(){
     //const output = keyIndex(this.props.messages, 1);
     const messages = this.props.messages.map((output,index)=><Message key={index} messageClass={output}/>);
    return(
     <div id="messenger">
         {messages}
+        <div style={{ float:"left", clear: "both" }}
+             ref={(el) => { this.messagesEnd = el; }}>
+        </div>
     </div>
     );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,14 +13,29 @@ import Playground from "../components/Playground";
 import InputLine from "../components/InputLine";
 import Messenger from "../components/Messenger";
 
+
+function addMessage(newMessage) {
+    return (previousState, currentProps) => {
+    	console.log("____________ changed messages");
+       return { ...previousState, messages: previousState.messages.concat(newMessage) };
+    };
+}
+
 export default class Home extends React.Component{
 
 	constructor(){
 		super();
 		this.goalTree= new TreeClass("cus u told me to");
 		this.lastGoal = "cus u told me to";
-		this.messages = [];
+		this.sampleCommands = ["Move R on top of A", "why move N to Y?", "How did you do that?", "how did you clear the top of A?","Pls move box Y ontop of N." ];
+		this.sampleIndex = 0;
 		this.state={
+			context: {
+				command:"",
+				A:"",
+				B:""
+			},
+			messages:[],
 			A:"",
 			B:"",
 			command:"Move R on top of A",
@@ -70,11 +85,16 @@ export default class Home extends React.Component{
 		}
 
 		proccess(){
-		//this.messages=[""];
-		this.messages.push(new MessageClass(this.state.command, "user", 0));
+		//this.setState({messages:this.state.messages.concat(new MessageClass(this.state.command, "user", 0))});
+		if(this.state.command==""){
+			return;
+		}
+		this.sampleIndex+=1;
+		this.setState({command:this.sampleCommands[this.sampleIndex]});
+		this.setState(addMessage(new MessageClass(this.state.command, "user", 0)));
 		const command=this.state.command.replace(/[^a-zA-Z ]/g, "");
 		const part = command.split(" ");
-		const simple = part.map((word)=>{
+		let simple = part.map((word)=>{
 			if(word.length>1){
 				return (word.toLowerCase());
 			}else{
@@ -85,14 +105,14 @@ export default class Home extends React.Component{
 			return (word=='reset'||word=='help'||word=='how'||word=='why'||word=='move'||word=='clear'||word=='that'||(word.length==1&&
 				(word=='R'||word=='Y'||word=='A'||word=='N'||word=='q'||word=='i'||word=='y'||word=='u')));
 		});
-		console.log(simple);
+		let A_ID;
+		let B_ID;
+
 		if(simple.length==0||!(simple[0]=='reset'||simple[0]=='why'||simple[0]=='how'||simple[0]=='move'||simple[0]=='help')){
-			this.setState({...this.state, answer:"sorry wat?"});
-			this.messages.push(new MessageClass("sorry wat?", "me", 2*Math.random()));
+			this.setState(addMessage(new MessageClass("sorry wat?", "me", 2*Math.random())));
 			return;
-		}else if(simple.find((word)=>{return word=='that';})!==undefined){
-			this.setState({...this.state, answer:"sorry what do you mean by 'that'? :p"});
-			this.messages.push(new MessageClass("sorry what do you mean by 'that'? :p", "me", 2*Math.random()));
+		}else if(simple.find((word)=>{return word=='that';})!==undefined && this.state.context.command==""){
+			this.setState(addMessage(new MessageClass("sorry what do you mean by 'that'? :p", "me", 2*Math.random())));
 			return;
 		}else if(simple.find((word)=>{return word=='reset';})!==undefined){
 			this.initializeBoxes();
@@ -100,27 +120,48 @@ export default class Home extends React.Component{
 		}else if(simple[0]=='help'){
 			const manual = "Send 'Help!' for help. Send 'Reset!' to reset the boxes. Ask me to move blocks on top of other blocks, or how/why I did stuff."
 			+" For example, 'pls move block R on top of A casey. thanks!'";
-			this.messages.push(new MessageClass(manual , "me", 0));
-			this.setState({...this.state, answer:manual});
+			this.setState(addMessage(new MessageClass(manual , "me", 0)));
 			return;
 		}
-		const first_word=simple[0];
+		console.log(simple);
+		let first_word=simple[0];
+		A_ID = simple.find((word)=>{return word.length==1;});
+		B_ID = simple.slice(simple.indexOf(A_ID)+1).find((word)=>{return word.length==1;});
+		
+		if(simple.find((word)=>{return word=='that';})!==undefined && this.state.context.command!==""){
+			console.log("editting")
+			const question = simple.find((word)=>{return word=='move'||word=='clear';});
+			if (question==undefined){
+				simple.splice(1,1,this.state.context.command);
+			}
+			if (A_ID==undefined){
+				A_ID=this.state.context.A;
+				simple=[...simple,A_ID];
+			}
+			if (B_ID==undefined){
+				B_ID=this.state.context.B;
+				simple=[...simple,B_ID];
+			}
+		}
+			console.log(simple);
 
 		if(first_word=='move'){
 			this.goalTree= new TreeClass("cus u told me to");
 			this.lastGoal = "cus u told me to";
-			const A_ID = simple.find((word)=>{return word.length==1;});
-			simple.splice(simple.indexOf(A_ID),1);
-			const B_ID = simple.find((word)=>{return word.length==1;});
 			if(A_ID==undefined||B_ID==undefined){
-				this.setState({...this.state, answer:"move what on top of where?"});
-				this.messages.push(new MessageClass("move what on top of where?", "me", 2*Math.random()));
-				return;}
+				this.setState(addMessage(new MessageClass("move what on top of where?", "me", 2*Math.random())));
+				return;
+			}
 				const responses = ['ok.','fine.','sure thing boss!','whatever...','lol sure','kk', ' so bossy -_-'];
 				let index =Math.floor(Math.random()*(responses.length));
 				const reponse = responses[index];
-				this.messages.push(new MessageClass(reponse, "me", 0));
-				this.setState({...this.state, A:A_ID, B:B_ID });
+				this.setState(addMessage(new MessageClass(reponse, "me", 0)));
+				this.setState({A:A_ID, B:B_ID });
+				this.setState({context:{
+					command:"move",
+					A:A_ID,
+					B:B_ID,
+				}});
 				this.move(A_ID,B_ID, ()=>{console.log("end")});
 
 			}else if(first_word=='why'){
@@ -146,6 +187,11 @@ export default class Home extends React.Component{
 					console.log("can't find A or B: "+A+" "+B);
 					question = null;
 				}else{
+					this.setState({context:{
+						command:"move",
+						A:A,
+						B:B,
+					}});
 					question = "move "  + A+ " to "+B; 
 					console.log(question);
 				}
@@ -153,9 +199,15 @@ export default class Home extends React.Component{
 				if(A==null){
 					console.log("can't find A : "+A);
 					question = null;
+				}else{
+					this.setState({context:{
+						command:"clear",
+						A:A,
+						B:"none",
+					}});
+					question = "clear the top of "+A;
+					console.log(question);
 				}
-				question = "clear the top of "+A;
-				console.log(question);
 			}else{
 				question = null;
 			}
@@ -164,8 +216,7 @@ export default class Home extends React.Component{
 
 		findHow(question){
 			if(question==null){
-				this.setState({...this.state, answer:"sorry wat?"});
-				this.messages.push(new MessageClass("sorry wat?", "me", 2*Math.random()));
+				this.setState(addMessage(new MessageClass("sorry wat?", "me", 2*Math.random())));
 			}else{
 				let children = [];
 				let task = null;
@@ -173,28 +224,36 @@ export default class Home extends React.Component{
 					{	task =node;
 						children = node.children;
 					},(nodeValue)=>{return nodeValue==question});
+
 				if(task==null){
-					this.setState({...this.state, answer:"hmmm did I do that?"});
-					this.messages.push(new MessageClass("hmmm did I do that?", "me", 2*Math.random()));
+
+					this.setState(addMessage(new MessageClass("hmmm did I do that?", "me", 2*Math.random())));
 				}
 				else if(children.length==0){
-					this.setState({...this.state, answer:"just kinda did it xD"});
-					this.messages.push(new MessageClass("just kinda did it xD", "me", 2*Math.random()));
+
+					this.setState(addMessage(new MessageClass("just kinda did it xD", "me", 2*Math.random())));
 				}else {
 					const how = children.map((node)=>{return node.value.replace("move","moved").replace("clear", "cleared").replace("grab", "grabbed");}).join(', ');
 					const endings = [' and kinda just did it...',' ...',' and like yeah',' and stuff',' and used my magic powers :p',' with confidence ;)', ' and did it'];
 					let index =Math.floor(Math.random()*(endings.length));
 					const end = endings[index];
-					this.setState({...this.state, answer:"I "+how+ end});
-					this.messages.push(new MessageClass("I "+how+ end, "me", 2*Math.random()));
+
+					this.setState(addMessage(new MessageClass("I "+how+ end, "me", 2*Math.random())));
 				}
 			}
+			var self=this;
+			setTimeout(function() {
+				self.forceUpdate();
+			}, 1000);
+			setTimeout(function() {
+				self.forceUpdate();
+			}, 3000);
 		}
 
 		findWhy(question){
 			if(question==null){
-				this.setState({...this.state, answer:"sorry wat?"});
-				this.messages.push(new MessageClass("sorry wat?", "me", 2*Math.random()));
+
+				this.setState(addMessage(new MessageClass("sorry wat?", "me", 2*Math.random())));
 			}else{
 				let parent = null;
 				this.goalTree.findDF((node)=>
@@ -202,17 +261,24 @@ export default class Home extends React.Component{
 					parent = node.parent;
 				},(nodeValue)=>{return nodeValue==question});
 				if(parent==null){
-					this.setState({...this.state, answer:"hmmm did I do that?"});
-					this.messages.push(new MessageClass("hmmm did I do that?", "me", 2*Math.random()));
+
+					this.setState(addMessage(new MessageClass("hmmm did I do that?", "me", 2*Math.random())));
 				}
 				else if(parent.value=="cus u told me to"){
-					this.setState({...this.state, answer:"cus u told me to."});
-					this.messages.push(new MessageClass("cus u told me to.", "me", 2*Math.random()));
+
+					this.setState(addMessage(new MessageClass("cus u told me to.", "me", 2*Math.random())));
 				}else {
-					this.setState({...this.state, answer:"to "+parent.value});
-					this.messages.push(new MessageClass("to "+parent.value, "me", 2*Math.random()));
+
+					this.setState(addMessage(new MessageClass("to "+parent.value, "me", 2*Math.random())));
 				}
 			}
+			var self=this;
+			setTimeout(function() {
+				self.forceUpdate();
+			}, 1000);
+			setTimeout(function() {
+				self.forceUpdate();
+			}, 3000);
 		}
 
 		addGoal(goal){
@@ -234,10 +300,10 @@ export default class Home extends React.Component{
 		if(this.above(A, A.width).size!==0){
 			//this.addGoal("grab "+A_ID);
 			this.addGoal("clear the top of "+A_ID);
-			self.clearTop(A_ID, B_ID, A_ID, B_ID, A.width);
+			this.clearTop(A_ID, B_ID, A_ID, B_ID, A.width);
 		}else if(this.above(B, A.width).size!==0){
 			this.addGoal("clear the top of "+B_ID);
-			self.clearTop(B_ID, A_ID, A_ID, B_ID, A.width);
+			this.clearTop(B_ID, A_ID, A_ID, B_ID, A.width);
 		}else{
 			return new Promise(function(resolve, reject) {
 				self.phyMove(A_ID,B_ID,()=>{
@@ -425,12 +491,10 @@ chatIntro(delay){
 	const manual = "If you ask me to 'move' a box on top of another box, I'll try my best. Feel free to ask how or why I do stuff, but try to be specific :p";
 	const self = this;
 	setTimeout(function() {
-		self.messages.push(new MessageClass(hi , "me", 0));
-		self.setState({...this.state, answer:hi});
+		self.setState(addMessage(new MessageClass(hi , "me", 0)));
 	}, delay);
 	setTimeout(function() {
-		self.messages.push(new MessageClass(manual , "me", 0));
-		self.setState({...this.state, answer:manual});
+		self.setState(addMessage(new MessageClass(manual , "me", 0)));
 	}, delay+3000);
 }
 
@@ -476,9 +540,9 @@ componentWillUnmount(){
 			</div>
 			<div class="chat-wrapper col-4">
 			<div class="chatInput">
-			<Messenger id="messages" messages={this.messages}/>
+			<Messenger id="messages" messages={this.state.messages}/>
 			<InputLine changeCommand={this.changeCommand.bind(this)} 
-			command={this.state.command} 
+			command={this.state.command}
 			proccess={this.proccess.bind(this)} 
 			look={this.showBoxes.bind(this)} />
 			</div>
